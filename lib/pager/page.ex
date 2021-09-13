2972cff2f81f8612beef946236f1c35c2ddf3ba6
Ben Coppock <ben@goodpixelmedia.com>
2021-09-12 22:37:23 -0700

    Add documentation to Page struct

--- lib/pager/page.ex
+++ lib/pager/page.ex
@@ -1,4 +1,18 @@
 defmodule Pager.Page do
-  @enforce_keys [:records, :has_previous_page, :has_next_page, :start_cursor, :end_cursor]
-  defstruct [:records, :has_previous_page, :has_next_page, :start_cursor, :end_cursor]
+  @moduledoc """
+  A single page of results.
+
+  ## Fields
+
+    * `raw_results` — rows in the form `{cursor_values, record}` where `cursor_values` is the list
+      of values to be used for generating a cursor. Note that in cases where coalescing or other
+      manipulation was performed for the sake of pagination, the cursor values will reflect
+      that manipulation, while the record itself will by default not.
+    * `has_previous_page` — whether or not there is a previous page of results.
+    * `has_next_page` — whether or not there is a subsequent page of results.
+    * `start_cursor` — a cursor representing the first record in this page of results.
+    * `end_cursor` — a cursor representing the last record in this page of results.
+  """
+  @enforce_keys [:raw_results, :has_previous_page, :has_next_page, :start_cursor, :end_cursor]
+  defstruct [:raw_results, :has_previous_page, :has_next_page, :start_cursor, :end_cursor]
 end
